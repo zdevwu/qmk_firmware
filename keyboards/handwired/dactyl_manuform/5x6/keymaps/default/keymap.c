afa273917c5671f265fc3470882d7fd1f41933b7
Zhuyuan <zdevwu@gmail.com>
2019-05-11 18:27:20 +0100

    hold shift and numlock

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -7,32 +7,32 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define _WINDOW_SWITCH 3
+#define _WITH_ALT 3
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
-#define WIN_ALT LM(_WINDOW_SWITCH, MOD_LALT)
+#define WITH_ALT LM(_WITH_ALT, MOD_LALT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
      KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_EQL,
      KC_ESC , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,RSFT_T(KC_QUOT),
+     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,RCTL_T(KC_BSLASH),
                       KC_LBRC,KC_RBRC,                                                       KC_BSPC, KC_DEL,
                                       KC_SPC,                                         KC_ENT,
                                       KC_HOME, RAISE,                        KC_RCTL, KC_END,
-                                      WIN_ALT, LOWER,                        KC_LGUI, KC_TAB
+                                      WITH_ALT,LOWER,                        KC_LGUI, KC_TAB
   ),
 
   [_LOWER] = LAYOUT_5x6(
 
-     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
-     _______,_______,_______,_______,_______,KC_PGUP,                        _______, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,_______,
-     _______,_______,KC_MPRV,KC_MPLY,KC_MNXT,KC_PGDN,                        _______, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,_______,
-     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,_______,
-                     _______,_______,                                                         KC_P0 ,_______,
+     KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PPLS,
+     _______,_______,_______,_______,_______,KC_PGUP,                        _______, KC_P7 , KC_P8 , KC_P9 ,KC_NLCK,KC_PMNS,
+     _______,_______,KC_MPRV,KC_MPLY,KC_MNXT,KC_PGDN,                        _______, KC_P4 , KC_P5 , KC_P6 ,KC_PAST,_______,
+     _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_PSLS,KC_EQL ,
+                     _______,_______,                                                         KC_P0 ,KC_PENT,
                                      _______,                                        _______,
                                      _______,_______,                        _______,_______,
                                      _______,_______,                        _______,_______
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      _______,_______,                      _______,_______
   ),
 
-  [_WINDOW_SWITCH] = LAYOUT_5x6(
+  [_WITH_ALT] = LAYOUT_5x6(
      _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_UP,KC_DOWN,                                                      KC_LEFT,KC_RGHT,
                                      _______,                                      _______,
                                      _______,_______,                      _______,_______,
-                                     _______,_______,                      _______,_______
+                                     _______,_______,                      KC_SPC ,_______
   ),
 };
 
