41a57004b352ca16637d3b9af7e9d041afc5edf9
Zhuyuan <zdevwu@gmail.com>
2019-05-12 22:03:53 +0100

    with alt + function keys

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -22,11 +22,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,RCTL_T(KC_BSLASH),
                       KC_LBRC,KC_RBRC,                                                       KC_BSPC, KC_DEL,
                                       KC_SPC,                                         KC_ENT,
-                                      KC_HOME, RAISE,                        KC_RCTL, KC_END,
+                                      KC_HOME, RAISE,                        KC_LALT, KC_END,
                                       WITH_ALT,LOWER,                        KC_LGUI, KC_TAB
   ),
 
-  [_LOWER] = LAYOUT_5x6(
+  [_RAISE] = LAYOUT_5x6(
 
      KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PPLS,
      _______,_______,_______,_______,_______,KC_PGUP,                        _______, KC_P7 , KC_P8 , KC_P9 ,KC_NLCK,KC_PMNS,
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
 
-  [_RAISE] = LAYOUT_5x6(
+  [_LOWER] = LAYOUT_5x6(
      KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                      KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
      _______,_______,_______,_______,_______,KC_PGUP,                      _______,_______,KC_INS ,_______,KC_PSCR,KC_SLCK,
      _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_PGDN,                      KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,_______,
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_WITH_ALT] = LAYOUT_5x6(
-     _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
+     KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                      KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
      _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                      _______,_______,_______,_______,_______,_______,
